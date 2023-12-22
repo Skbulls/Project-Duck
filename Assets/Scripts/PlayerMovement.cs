0f64c5297c97f1a04d2ff475f4564cdcfaf5f5b4
Skbulls <107372990+Skbulls@users.noreply.github.com>
2023-12-21 22:11:14 -0800

    Working PlayerMovement
    The PlayerMovement Now Works in FPV Mode

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,9 +6,9 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
 
-    public float speed = 12f;
+    public float speed = 15f;
     public float gravity = -9.81f * 2;
-    public float jumpHeight = 3f;
+    public float jumpHeight = 2.5f;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
